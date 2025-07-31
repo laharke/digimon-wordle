31659d9248d10040f956a1f1a56403a53c966ab8
laharke <ronconimauro@yahoo.com.ar>
2025-07-31 18:00:35 -0300

    input autocmplete

--- wordle/static/js/script.js
+++ wordle/static/js/script.js
@@ -1,1 +1,63 @@
 console.log("Digimon Wordle loaded!");
+
+let digimons = [];
+
+// Loop para filtrar lo que ponga el usuario , necesito poder sleccionarlo 
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/static/data/digimons.json')
+        .then(res => res.json())
+        .then(data => {
+            digimons = data;
+    });
+
+    const input = document.getElementById('digimon-search');
+    const resultsList = document.getElementById('results');
+
+    input.addEventListener('input', () => {
+    const value = input.value.trim().toLowerCase();
+    resultsList.innerHTML = '';
+
+    if (value === '') return;
+
+    const matches = digimons.filter(d => d.name.toLowerCase().startsWith(value)).slice(0, 20);
+
+
+    // Fillea el div abajo
+    matches.forEach(d => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item clickable-digimon';
+        li.textContent = `${d.name} — Type: ${d.type}, Attribute: ${d.attribute}`;
+        li.dataset.id = d.id;
+        resultsList.appendChild(li);
+    });
+    });
+
+
+    // Even listener para detectar el click en el que seleccione el usario
+    resultsList.addEventListener('click', (e) => {
+    const clicked = e.target;
+    if (clicked.classList.contains('clickable-digimon')) {
+        const digimonId = clicked.dataset.id;
+        fetchDigimonDetails(digimonId);
+    }
+  });
+});
+
+function fetchDigimonDetails(id) {
+  fetch(`https://digi-api.com/api/v1/digimon/${id}`)  // replace with your actual API endpoint
+    .then(res => res.json())
+    .then(data => {
+      console.log("Digimon API response:", data);
+
+      console.log(data['images'][0]['href'])
+      // You can now show this info in a new div or modal
+      //document.getElementById('digimon-info').innerText = JSON.stringify(data, null, 2);
+      document.getElementById('digimon-info').innerHTML = '<img src="' + data['images'][0]['href'] + '"/>'
+    })
+    .catch(err => {
+    document.getElementById('digimon-info').innerHTML = '<img src="https://digi-api.com/images/digimon/w/Gabumon.png" alt="Dinosaur"/>'
+
+      console.error("Error fetching Digimon data:", err);
+    });
+}
--- wordle/views.py
+++ wordle/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+import os
+import json
+
+
+
 
 
 
 
 # Create your views here.
 def index(request):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    with open(os.path.join(BASE_DIR, 'wordle','static' ,'data', 'digimons.json')) as f:
+        digimons = json.load(f)
+
+    #digimons[id]
+    #digimons[image]
+    print(digimons[0])
+
+    # Tal vez puedo agarrar toda la info y armamre un config file , la parseo y creo un json que sea
+    # {id, name, foto} y con esto armo el dropdown menu.
+    # Y cuando selecciona algo el usuario hago el submit y un llamdo ajax a la api para taer la info.
+    
     return render(request, 'index.html')
+
+# necesito testear si la api anda o ageraru n config file
